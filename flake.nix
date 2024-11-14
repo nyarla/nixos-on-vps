af46661aaff18761ab59d78f6d040701566f74e2
nyarla <nyarla@kalaclista.com>
2024-11-14 16:04:47 +0900

    chore(flake): use nixos-24.04 branch

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
 
     impermanence.url = "github:nix-community/impermanence";
