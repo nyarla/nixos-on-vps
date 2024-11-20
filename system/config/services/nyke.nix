101b4d54f98b8b44e329651d2d9d9aeedebb0a4c
nyarla <nyarla@kalaclista.com>
2024-11-20 13:45:50 +0900

    feat(service): add mackerel agent service

--- system/config/services/nyke.nix
+++ system/config/services/nyke.nix
@@ -1,4 +1,20 @@
 _: {
+  services.mackerel-agent = {
+    enable = true;
+    apiKeyFile = "/var/lib/mackerel-agent/apikey";
+    settings = {
+      display_name = "Nyke";
+      cloud_platform = "none";
+      filesystems = {
+        ignore = "/dev/vda[12]";
+        use_mountpoint = false;
+      };
+      interfaces = {
+        ignore = "ens[45]";
+      };
+    };
+  };
+
   services.caddy = {
     enable = true;
     virtualHosts = {
