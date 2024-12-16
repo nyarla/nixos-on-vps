5819413859d498a809d534c2811314d71f491518
nyarla <nyarla@kalaclista.com>
2024-12-16 11:43:19 +0900

    chore(flake): upgrade to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
 
     impermanence.url = "github:nix-community/impermanence";
