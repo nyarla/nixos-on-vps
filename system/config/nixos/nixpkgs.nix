94cd3ed65316ee6dbe8124bc5ba2e5b84298334c
nyarla <nyarla@kalaclista.com>
2025-01-19 17:17:16 +0900

    feat(nyke/nix): set automatic gc to nix

--- system/config/nixos/nixpkgs.nix
+++ system/config/nixos/nixpkgs.nix
@@ -9,5 +9,12 @@ _: {
     "@wheel"
   ];
 
+  nix.gc = {
+    automatic = true;
+    dates = "Mon *-*-* 4:00:00";
+    randomizedDelaySec = "3min";
+    options = "--delete-older-than 10d";
+  };
+
   system.stateVersion = "24.05";
 }
