4a5747129752163e0ae5fac4e3469753c4431400
nyarla <nyarla@kalaclista.com>
2025-03-28 13:14:11 +0900

    chore: use nixos-24.11-small instead of nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11-small";
     nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
 
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
