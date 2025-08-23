1d1001ccc0bf8e857891fb221a0f73d522f110c5
nyarla <nyarla@kalaclista.com>
2025-08-23 12:37:11 +0900

    chore(liresteam): update to latest revision

--- pkgs/litestream/default.nix
+++ pkgs/litestream/default.nix
@@ -5,16 +5,16 @@ buildGo124Module {
   src = fetchFromGitHub {
     owner = "benbjohnson";
     repo = "litestream";
-    rev = "6c35001a521ef832f83b45c4e056cfd719455086";
-    hash = "sha256-kwioOomGZAqmY5jvpCSPjp1JHmYrULl7LR6cvtOopuw=";
+    rev = "feee71f6a90b464ee74da337043f3ad5d59a6b05";
+    hash = "sha256-GdCiKiawVPFd+eNc5Yr1//nRg+WE/JjYo7uepAdn2bU=";
   };
 
-  vendorHash = "sha256-mpFiTONT27MdZNwAY+DNNWoY+fKDIddRitSzns1R8B8=";
+  vendorHash = "sha256-y2gI5dk/tgjmrSnUapPl3MpMdzUl49zSiG93EZtmCgw=";
 
   ldflags = [
     "-s"
     "-w"
-    "-X main.Version=6c35001"
+    "-X main.Version=feee71f"
   ];
 
   tags = [
