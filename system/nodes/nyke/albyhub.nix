da46d25e0cf3f34bd4285957722a84722ebef889
nyarla <nyarla@kalaclista.com>
2025-09-02 13:03:46 +0900

    chore(albyhub): update to v1.19.3

--- system/nodes/nyke/albyhub.nix
+++ system/nodes/nyke/albyhub.nix
@@ -6,7 +6,7 @@ in
 {
   virtualisation.oci-containers.containers."albyhub" = {
     autoStart = true;
-    image = "ghcr.io/getalby/hub:v1.18.5";
+    image = "ghcr.io/getalby/hub:v1.19.3";
     ports = [
       "${listen}:8080"
     ];
